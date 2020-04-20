8b9e278d7e531b79586bc7511943ea2f1575abf8
keik <keik@c-fo.com>
2020-04-21 08:00:32 +0900

    WIP

--- src/repolint.ts
+++ src/repolint.ts
@@ -43,7 +43,7 @@ const main = async () => {
   );
   // TODO: more parallelly
   await Promise.all(repos.map((a) => check(a)));
-  console.log(results);
+  console.log(results.map((a) => JSON.stringify(a)).join("\n"));
 };
 
 main();
