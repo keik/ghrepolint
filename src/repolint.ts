4c6736c3e4dcaca0c2c6c1cf771d70517170715f
keik <keik@c-fo.com>
2020-04-21 07:54:24 +0900

    WIP

--- src/repolint.ts
+++ src/repolint.ts
@@ -57,7 +57,7 @@ const report = (params: ReportParams) => {
 /**** check executor ****/
 const check = async (repo: Repo) => {
   d("check");
-  const checkers = [topicChecker, ciChecker];
+  const checkers = [topicChecker, ciChecker, branchProtectionChecker];
   await Promise.all(checkers.map((c) => c(repo)));
 };
 
@@ -107,6 +107,68 @@ const ciChecker = async (repo: Repo) => {
       rule: "ci/required",
       repo: repo.name,
       message: `CI settings ${opts.path} is not exist.`,
+    });
+  }
+};
+
+const branchProtectionChecker = async (repo: Repo) => {
+  d("branchProtectionChecker");
+  const opts = {
+    required: {
+      required_status_checks: {
+        contexts: [
+          "ci/circleci: rspec",
+          "ci/circleci: test_js",
+          "ci/circleci: eslint_flow",
+          "ci/circleci: danger",
+          "danger/danger",
+        ],
+      },
+      required_pull_request_reviews: { require_code_owner_reviews: true },
+    },
+  };
+  try {
+    const {
+      data: rawBranchProtection,
+    } = await octokit.repos.getBranchProtection({
+      owner: repo.owner,
+      repo: repo.name,
+      branch: repo.default_branch,
+    });
+    if (opts.required.required_status_checks) {
+      if (opts.required.required_status_checks.contexts) {
+        opts.required.required_status_checks.contexts.forEach((a) => {
+          if (
+            !rawBranchProtection.required_status_checks.contexts.includes(a)
+          ) {
+            report({
+              rule: "branchProtection/required",
+              repo: repo.name,
+              message: `Required status checks '${a}' is not exist.`,
+            });
+          }
+        });
+      }
+    }
+    if (opts.required.required_pull_request_reviews) {
+      if (
+        opts.required.required_pull_request_reviews
+          .require_code_owner_reviews &&
+        !rawBranchProtection.required_pull_request_reviews
+          .require_code_owner_reviews
+      ) {
+        report({
+          rule: "branchProtection/required",
+          repo: repo.name,
+          message: `Pull request reviews are required.`,
+        });
+      }
+    }
+  } catch (e) {
+    report({
+      rule: "branchProtection/required",
+      repo: repo.name,
+      message: `Branch protection is not exist. (default branch: ${repo.default_branch})`,
     });
   }
 };
