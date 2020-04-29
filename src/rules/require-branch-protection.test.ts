838e88d49c041cb9bc2c241a19c6c79f4fb30c4b
keik <keik@c-fo.com>
2020-04-29 16:07:28 +0900

    :+1: Refactor.

--- src/rules/require-branch-protection.test.ts
+++ src/rules/require-branch-protection.test.ts
@@ -1,9 +1,12 @@
 import { report } from "../reporter";
+import * as Repositories from "../repositories";
 
-import { check } from "./require-branch-protection";
+import Rule from "./require-branch-protection";
 
+jest.mock("../repositories");
 jest.mock("../reporter");
 
+const mockedRepositories = Repositories as jest.Mocked<typeof Repositories>;
 const mockedReport = report as jest.MockedFunction<typeof report>;
 
 const repository = {
@@ -15,37 +18,43 @@ const repository = {
 };
 
 test("with empty config", async () => {
-  check(
-    { repository, ruleConfig: {} },
-    {
-      requireCodeOwnerReviews: false,
-      requiredStatusChecks: ["a"],
-    }
-  );
+  mockedRepositories.getBranchProtection.mockResolvedValue({
+    requireCodeOwnerReviews: false,
+    requiredStatusChecks: ["a"],
+  });
+
+  await Rule.checker({ repository, ruleConfig: {} });
+
   expect(mockedReport.mock.calls.length).toBe(0);
 });
 
 test("with requireCodeOwnerReviews: true", async () => {
-  check(
-    { repository, ruleConfig: { requireCodeOwnerReviews: true } },
-    {
-      requireCodeOwnerReviews: false,
-      requiredStatusChecks: ["a"],
-    }
-  );
+  mockedRepositories.getBranchProtection.mockResolvedValue({
+    requireCodeOwnerReviews: false,
+    requiredStatusChecks: ["a"],
+  });
+
+  await Rule.checker({
+    repository,
+    ruleConfig: { requireCodeOwnerReviews: true },
+  });
+
   expect(mockedReport.mock.calls.map((args) => args[0].message)).toEqual([
     "Pull request reviews are required.",
   ]);
 });
 
 test('with requiredStatusChecks: ["a", "b", "c"]', async () => {
-  check(
-    { repository, ruleConfig: { requiredStatusChecks: ["a", "b", "c"] } },
-    {
-      requireCodeOwnerReviews: false,
-      requiredStatusChecks: ["a"],
-    }
-  );
+  mockedRepositories.getBranchProtection.mockResolvedValue({
+    requireCodeOwnerReviews: false,
+    requiredStatusChecks: ["a"],
+  });
+
+  await Rule.checker({
+    repository,
+    ruleConfig: { requiredStatusChecks: ["a", "b", "c"] },
+  });
+
   expect(mockedReport.mock.calls.map((args) => args[0].message)).toEqual([
     "Required status checks 'b' is not exist.",
     "Required status checks 'c' is not exist.",
--- src/rules/require-branch-protection.ts
+++ src/rules/require-branch-protection.ts
@@ -4,31 +4,9 @@ import { BranchProtection, Context, Rule } from "../types";
 
 const RULE_NAME = "require-branch-protection";
 
-export const check = (ctx: Context, bp: BranchProtection): void => {
-  if (ctx.ruleConfig.requiredStatusChecks) {
-    ctx.ruleConfig.requiredStatusChecks.forEach((a: string) => {
-      if (!bp.requiredStatusChecks.includes(a)) {
-        report({
-          rule: RULE_NAME,
-          repo: ctx.repository.name,
-          message: `Required status checks '${a}' is not exist.`,
-        });
-      }
-    });
-  }
-
-  if (ctx.ruleConfig.requireCodeOwnerReviews && !bp.requireCodeOwnerReviews) {
-    report({
-      rule: RULE_NAME,
-      repo: ctx.repository.name,
-      message: `Pull request reviews are required.`,
-    });
-  }
-};
-
 const rule: Rule = {
   name: RULE_NAME,
-  checker: async function (ctx: Context): Promise<void> {
+  checker: async (ctx: Context): Promise<void> => {
     let branchProtection: BranchProtection | null = null;
     try {
       branchProtection = await Repositories.getBranchProtection(ctx.repository);
@@ -41,7 +19,28 @@ const rule: Rule = {
       return;
     }
 
-    check(ctx, branchProtection);
+    if (ctx.ruleConfig.requiredStatusChecks) {
+      ctx.ruleConfig.requiredStatusChecks.forEach((a: string) => {
+        if (!branchProtection?.requiredStatusChecks.includes(a)) {
+          report({
+            rule: RULE_NAME,
+            repo: ctx.repository.name,
+            message: `Required status checks '${a}' is not exist.`,
+          });
+        }
+      });
+    }
+
+    if (
+      ctx.ruleConfig.requireCodeOwnerReviews &&
+      !branchProtection.requireCodeOwnerReviews
+    ) {
+      report({
+        rule: RULE_NAME,
+        repo: ctx.repository.name,
+        message: `Pull request reviews are required.`,
+      });
+    }
   },
 };
 
