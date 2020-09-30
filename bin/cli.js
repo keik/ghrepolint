e0b4804b40a38d461c1fbbc2f374721b44cbd62f
keik <keik@c-fo.com>
2020-10-01 07:46:46 +0900

    :sparkles: Accept rule option from CLI.

--- bin/cli.js
+++ bin/cli.js
@@ -20,13 +20,39 @@ const argv = yargs
       required: false,
     },
   })
+  .options({
+    rule: {
+      description: "Specify rule.",
+      required: false,
+    },
+  })
+  .example(
+    "repolint github --rule 'require-branch-protection: { \"requireCodeOwnerReviews\": true }'"
+  )
   .demandCommand(1, 1).argv;
 
 const configPath = path.join(process.cwd(), ".repolintrc.js");
 const config = fs.existsSync(configPath) ? require(configPath) : {};
 
+const rulesFromConfig = config.rules;
+const rulesFromArgv = (Array.isArray(argv.rule)
+  ? argv.rule
+  : [argv.rule]
+).reduce((acc, ruleString) => {
+  try {
+    const { name, value } = ruleString.match(
+      "^(?<name>.+?):(?<value>.+)$"
+    ).groups;
+    return { ...acc, [name]: JSON.parse(value) };
+  } catch (e) {
+    throw new TypeError(
+      "Invalid value for `rule` option: must be formed like '<name>: <value with JSON>'"
+    );
+  }
+}, {});
+
 repolint({
   target: argv._[0],
-  config: config,
+  config: { ...config, rules: { ...rulesFromConfig, ...rulesFromArgv } },
   verbose: argv.verbose,
 });
